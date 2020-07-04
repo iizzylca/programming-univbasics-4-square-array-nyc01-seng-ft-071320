9985a30a54c992b0964d79e7cfbc9f4785bbbc59
Israel Canessa <israellcanessa@gmail.com>
2020-07-04 08:22:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,15 +2,12 @@ require 'pry'
 
 array = [1,2,3]
 
-def square_array(array)
-  count = 0
-  new_array = []
-  while count < array.length do
-    new_array.push(array[count] ** 2)
-    count += 1
-  end
-new_array
-end
 
 def square_array(array)
   new_array = []
+  array.length.times { |index|
+    new_array.push(array[index] ** 2)
+  }
+  end
+end
+  
