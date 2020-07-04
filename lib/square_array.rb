7c72b71a7a70986ca8fe268fca60903d43563bda
Israel Canessa <israellcanessa@gmail.com>
2020-07-04 08:29:58 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,8 +4,7 @@ array = [1,2,3]
 
 def square_array(array)
   new_array = []
-  array.length.times {
-    |index|
+  array.length.times {|index|
     new_array.push(array[index] ** 2)
   }
 new_array
