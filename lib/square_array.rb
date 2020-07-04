b8e91a47a06c6a52b07dc65bcd5a740e5a8f7e8c
Israel Canessa <israellcanessa@gmail.com>
2020-07-04 08:17:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,7 +6,7 @@ def square_array(array)
   count = 0
   new_array = []
   while count < array.length do
-    new_array.push(array[counter] ** 2)
+    new_array.push(array[count] ** 2)
     count += 1
   end
 new_array
